a7814bc84fc9a869da6a3d8d6bc59395c67f80e7
pethoalpar <pethoalpar@gmail.com>
2017-06-01 15:55:30 +0300

    Initial Commit

---
+++ IOSHttpCall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSHttpCall
+//
+//  Created by Petho Alpar on 01/06/2017.
+//  Copyright © 2017 Petho Alpar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
