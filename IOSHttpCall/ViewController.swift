7f5b8da2b56143484f6c1f5ad3a2719bfc15f805
pethoalpar <pethoalpar@gmail.com>
2017-06-01 21:29:17 +0300

    First commit

--- IOSHttpCall/ViewController.swift
+++ IOSHttpCall/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func get(_ sender: Any) {
+        let url = URL(string: "http://192.168.0.104:7676?data="+textField.text!)
+        let task = URLSession.shared.dataTask(with: url!){data, response, error in
+            guard error == nil else{
+                DispatchQueue.main.async {
+                    self.textView.text = error! as? String
+                }
+                return
+            }
+            guard let data = data else{
+                DispatchQueue.main.async {
+                    self.textView.text = "The answer is empty"
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.textView.text = NSString(data: data,encoding: String.Encoding.utf8.rawValue) as String?
+            }
+        }
+        task.resume()
+    }
 
+    @IBAction func post(_ sender: Any) {
+        var request = URLRequest(url: URL(string: "http://192.168.0.104:7676" )!)
+        request.httpMethod = "POST"
+        let postStr = textField.text
+        request.httpBody = postStr?.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request){data, response, error in
+            guard error == nil else{
+                DispatchQueue.main.async {
+                    self.textView.text = error! as? String
+                }
+                return
+            }
+            guard let data = data else{
+                DispatchQueue.main.async {
+                    self.textView.text = "The answer is empty"
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.textView.text = NSString(data: data,encoding: String.Encoding.utf8.rawValue) as String?
+            }
+        }
+        task.resume()
+
+    }
+    @IBAction func put(_ sender: Any) {
+        var request = URLRequest(url: URL(string: "http://192.168.0.104:7676" )!)
+        request.httpMethod = "PUT"
+        let postStr = textField.text
+        request.httpBody = postStr?.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request){data, response, error in
+            guard error == nil else{
+                DispatchQueue.main.async {
+                    self.textView.text = error! as? String
+                }
+                return
+            }
+            guard let data = data else{
+                DispatchQueue.main.async {
+                    self.textView.text = "The answer is empty"
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.textView.text = NSString(data: data,encoding: String.Encoding.utf8.rawValue) as String?
+            }
+        }
+        task.resume()
+    }
 }
 
